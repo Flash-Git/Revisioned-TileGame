4f47fb0a17dcdacd38f878c369243d25b635720f
Flash <flash.jaq@gmail.com>
2017-11-16 09:13:45 +0000

    Updated Version Number

--- src/dev/flash/tilegame/Launcher.java
+++ src/dev/flash/tilegame/Launcher.java
@@ -3,7 +3,7 @@ package dev.flash.tilegame;
 public class Launcher {
 	
 	public static void main(String[] args) {
-		Game game = new Game("Tile Game - Alpha v1.45", 1100, 600);
+		Game game = new Game("Tile Game - Alpha v2", 1100, 600);
 		game.start();
 	}
 }
